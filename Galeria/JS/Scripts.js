a1887f1082ed4d05ce5367d8bba66be8323d2a34
Gabriel <Gabriel@gmail.com>
2021-07-29 12:21:25 -0400

    Mobile version lista

--- Galeria/JS/Scripts.js
+++ Galeria/JS/Scripts.js
@@ -1,2 +1,7 @@
-let Variable = 'HOLA MUNDO'
-console.log(Variable)
+const act = document.getElementById('menuh');
+const act_2 = document.getElementById('menucel');
+
+function cambio() {
+    act.classList.toggle('act');
+    act_2.classList.toggle('act_2');
+};
